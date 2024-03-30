e1bc8abb49c182165ab0ed90c8fcaf15c8f32ca0
LeilaKarzand <l.karzand@yahoo.com>
2024-03-30 12:36:21 +0330

    move hash_password

--- logics/hash.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-
-def hash_password(password: str):
-    # Use SHA-256 hash function
-    hashed = hashlib.sha256(password.encode()).hexdigest()
-    return hashed
--- logics/utils.py
+++ logics/utils.py
@@ -1,6 +1,13 @@
+import hashlib
 import re
 
 
 def camel_to_snake(name):
     name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+
+def hash_password(password: str):
+    # Use SHA-256 hash function
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    return hashed
