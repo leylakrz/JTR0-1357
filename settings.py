6e32c94c7580ed2f51f119bf13c3f98e5832c6a5
LeilaKarzand <l.karzand@yahoo.com>
2024-03-30 11:13:19 +0330

    update settings

--- settings.py
+++ settings.py
@@ -1,5 +1,22 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    pass
+    # Postgres
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB_NAME: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_URL: str = ""
+    POSTGRES_ASYNC_URL: str = ""
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.POSTGRES_URL = f'postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB_NAME}'
+        self.POSTGRES_ASYNC_URL = f'postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB_NAME}'
+
+
+settings = Settings()
