7f79690cbb9abda673831e580d511a5f6b14a759
LeilaKarzand <l.karzand@yahoo.com>
2024-03-31 20:26:32 +0330

    remove redundant code

--- resources/postgres/session.py
+++ resources/postgres/session.py
@@ -8,12 +8,6 @@ engine = create_async_engine(settings.POSTGRES_ASYNC_URL, echo=True, future=True
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-async def init_postgres_db():
-    async with engine.begin() as conn:
-        # await conn.run_sync(SQLModel.metadata.drop_all)
-        await conn.run_sync(SQLModel.metadata.create_all)
-
-
 async def get_postgres_async_session() -> AsyncSession:
     async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     async with async_session() as session:
